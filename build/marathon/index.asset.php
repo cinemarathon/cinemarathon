585e58123aafdbfa31409b353966058d8f073141
Charlie Merland <charlie@caercam.org>
2023-11-06 13:03:44 +0100

    Remove build from repo

--- build/marathon/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '859b7524474bbc03114a');
