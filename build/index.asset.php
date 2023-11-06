94bde3aedf70be69c39e6de52628fb85bd0590f3
Charlie Merland <charlie@caercam.org>
2023-11-06 13:03:44 +0100

    + production build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '854ecec8ce527397e22c');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e2a7a473fd05ba9e32b7');
