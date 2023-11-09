c163dd395296095716569780f6078dfe36521908
Charlie Merland <charlie@caercam.org>
2023-11-09 01:59:25 +0100

    Use webpack hot refresh

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
+
+if ( process.env.NODE_ENV !== 'production' ) {
+	defaultConfig.devServer.allowedHosts = [ 'www.wpmovielibrary.local', 'www.cinemarathons.local', 'localhost', '127.0.0.1' ];
+}
+
+module.exports = defaultConfig;
